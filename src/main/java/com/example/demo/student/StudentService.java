bbb58a3453ac6468a368db9c8701daa31e86f882
jdilsham <janithadilsham@gmail.com>
2025-11-03 20:55:03 +0530

    add

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -9,13 +10,14 @@ import java.util.List;
 @Component
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent(){
-        return List.of(
-                new Student(1L,
-                        "Janitha",
-                        "janitha@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,5),
-                        23)
-        );
+        return studentRepository.findAll();
     }
 }
