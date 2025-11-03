b4e9365dd142d8c0fc35b6ece41a35f5164a2b7e
jdilsham <janithadilsham@gmail.com>
2025-11-03 12:02:58 +0530

    add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,7 +18,9 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 	}
     @GetMapping
-    public String hello(){
-        return "hello world";
+    public List<Student> hello(){
+        return List.of(
+                new Student(1L,"Janitha","janitha@gmail.com", LocalDate.of(2000, Month.JANUARY,5),23)
+        );
     }
 }
